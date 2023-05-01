acf30f4c56ae3465d115850831e962e0bdba0212
Hossam Elwahsh <hacker2894@gmail.com>
2023-05-01 23:22:11 +0300

    update (SPI): both ECUs (ATM: master, CARD: slave)

--- Simple_ATM_Machine/ATM_ECU/APP/app_program.c
+++ Simple_ATM_Machine/ATM_ECU/APP/app_program.c
@@ -4,7 +4,7 @@
  *   Created on: Apr 10, 2023
  *       Author: Hacker Kermit Team
  *  Description: This file contains all Application (APP) functions' implementation.
- */ 
+ */
 
 /* APP */
 #include "app_interface.h"
@@ -16,16 +16,36 @@
 /**
  *
  */
-void APP_initialization( void )
-{
+void APP_initialization(void) {
+    EXI_enablePIE(MASTER_REQ_RECEIVE_INT, MASTER_REQ_RECEIVE_SENSE);
+    EXI_intSetCallBack(MASTER_REQ_RECEIVE_INT,APP_trigger);
+    SPI_init();
 
+//    u8 data;
+//    SPI_receive(&data);
+    u8 test = 1;
+
+// 	SPI_send('H');
+// 	SPI_send('O');
+// 	SPI_send('S');
+// 	SPI_send('S');
+// 	SPI_send('A');
+// 	SPI_send('M');
+//     while(1)
+// 	{
+// 		SPI_send('b');
+// 	}
 }
 
 
 /**
  *
  */
-void APP_startProgram  ( void )
-{
+void APP_startProgram(void) {
+
+}
+
+void APP_trigger(void) {
+    // receive SPI data
 
 }
