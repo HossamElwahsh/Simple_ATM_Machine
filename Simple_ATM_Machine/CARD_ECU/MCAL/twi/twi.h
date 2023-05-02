3a8e7449423b0d70466321a25ebe2a68466ad702
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-05-02 23:46:29 +0300

    update function prototypes

--- Simple_ATM_Machine/CARD_ECU/MCAL/twi/twi.h
+++ Simple_ATM_Machine/CARD_ECU/MCAL/twi/twi.h
@@ -9,51 +9,28 @@
 #ifndef TWI_H_
 #define TWI_H_
 
-#include "../../LIB/std.h"
+
 #include "../../LIB/bit_math.h"
+#include "../../LIB/std.h"
 #include "twi_cfg.h"
-
-#define WRITE                  0
-#define READ                   1
-
-#define ACK                    0
-#define NACK                   1
-
-#define EVENT_OK                0
-#define START_NOT_SENT          1
-#define REPEATED_START_NOT_SENT 2
-#define SLAVE_W_ACK_SENT        3
-#define SLAVE_W_NACK_SENT       4
-#define SLAVE_R_ACK_SENT        5
-#define SLAVE_R_NACK_SENT       6
-#define SLAVE_RW_FAILED         7
-#define DATA_WRITE_ACK_SENT     8
-#define DATA_WRITE_NACK_SENT    9
-#define DATA_WRITE_FAILED       10
-#define DATA_READ_ACK_SENT      11
-#define DATA_READ_NACK_SENT     12
-#define DATA_READ_FAILED        13
 
 
 /* I2C Status Bits in the TWSR Register */
-#define START_STATE             0x08 // start has been sent
-#define REPEATED_START_STATE    0x10 // repeated start
-#define SLAVE_W_ACK_STATE       0x18 // Master transmit ( slave address + Write request ) to slave + Ack received from slave
-#define SLAVE_W_NACK_STATE      0x20
-#define SLAVE_R_ACK_STATE       0x40 // Master transmit ( slave address + Read request ) to slave + Ack received from slave
-#define SLAVE_R_NACK_STATE      0x48
-#define DATA_WRITE_ACK_STATE    0x28 // Master transmit data and ACK has been received from Slave.
-#define DATA_WRITE_NACK_STATE   0x30
-#define DATA_READ_ACK_STATE     0x50 // Master transmit data and ACK has been received from Slave.
-#define DATA_READ_NACK_STATE    0x58
+#define TW_START         0x08 // start has been sent
+#define TW_REP_START     0x10 // repeated start
+#define TW_MT_SLA_W_ACK  0x18 // Master transmit ( slave address + Write request ) to slave + Ack received from slave
+#define TW_MT_SLA_R_ACK  0x40 // Master transmit ( slave address + Read request ) to slave + Ack received from slave
+#define TW_MT_DATA_ACK   0x28 // Master transmit data and ACK has been received from Slave.
+#define TW_MR_DATA_ACK   0x50 // Master received data and send ACK to slave
+#define TW_MR_DATA_NACK  0x58 // Master received data but doesn't send ACK to slave
 
-void TWI_init(void); 
-u8 TWI_start(void);
-u8 TWI_repeatedStart(void);
+void TWI_Init(void);
+void TWI_Start(void);
+void TWI_Stop(void);
+void TWI_Write(u8 data);
+u8 TWI_Read_With_ACK(void); //read with send Ack
+u8 TWI_Read_With_NACK(void); //read without send Ack
+u8 TWI_Get_Status(void);
 
 
-u8 TWI_addressSelect(u8 u8_l_address, u8 u8_l_readWrite);
-u8 TWI_dataEvent(u8 *u8_l_data, u8 u8_l_readWrite, u8 u8_l_ack);
-void TWI_stop(void);
-u8 TWI_getStatus(void);
 #endif /* TWI_H_ */
