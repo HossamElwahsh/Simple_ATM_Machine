0a94f89beceeda873380b84aad7e7e023f7eba3d
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-05-04 00:03:56 +0300

    function_prototypes documentation

--- Simple_ATM_Machine/CARD_ECU/MCAL/twi/twi.h
+++ Simple_ATM_Machine/CARD_ECU/MCAL/twi/twi.h
@@ -1,13 +1,6 @@
-/*
- * twi.h
- *
- * Created: 5/1/2023 4:24:00 PM
- *  Author: Mahmoud Mowafey
- */ 
 
-
-#ifndef TWI_H_
-#define TWI_H_
+#ifndef I2C_H_
+#define I2C_H_
 
 
 #include "../../LIB/bit_math.h"
@@ -17,20 +10,86 @@
 
 /* I2C Status Bits in the TWSR Register */
 #define TW_START         0x08 // start has been sent
-#define TW_REP_START     0x10 // repeated start
+#define TW_REP_START     0x10 // repeated start 
 #define TW_MT_SLA_W_ACK  0x18 // Master transmit ( slave address + Write request ) to slave + Ack received from slave
 #define TW_MT_SLA_R_ACK  0x40 // Master transmit ( slave address + Read request ) to slave + Ack received from slave
 #define TW_MT_DATA_ACK   0x28 // Master transmit data and ACK has been received from Slave.
 #define TW_MR_DATA_ACK   0x50 // Master received data and send ACK to slave
 #define TW_MR_DATA_NACK  0x58 // Master received data but doesn't send ACK to slave
 
-void TWI_Init(void);
-void TWI_Start(void);
-void TWI_Stop(void);
-void TWI_Write(u8 data);
-u8 TWI_Read_With_ACK(void); //read with send Ack
-u8 TWI_Read_With_NACK(void); //read without send Ack
-u8 TWI_Get_Status(void);
+/*
+* initializing the I2C protocol
+*
+* Parameters
+*       	[in] void
+* Return
+*   	    [out] void
+*
+*/
+void TWI_init(void);
+/*
+* Send the start bit to start the I2C frame
+*
+* Parameters
+*       	[in] void
+* Return
+*   	[out] void
+*
+*/
+void TWI_start(void);
+/*
+* Send the stop bit to stop the I2C frame
+*
+* Parameters
+*       	[in] void
+* Return
+*   	[out] void
+*
+*/
+void TWI_stop(void);
+/*
+* write one byte via I2C
+*
+* Parameters
+*       	[in] data
+* Return
+*   	   [out] void
+*
+*/
+void TWI_write(u8 data);
+
+/*
+* read one byte with send Ack via I2C
+*
+* Parameters
+*       	[in] void
+* Return
+*   	Received byte
+*
+*/
+u8 TWI_readWithAck(void); //read with send Ack
+
+/*
+* read one byte with send NAck via I2C
+*
+* Parameters
+*       	[in] void
+* Return
+*   	Received byte
+*
+*/
+u8 TWI_readWithNAck(void); //read without send Ack
+
+/*
+* check the last operation via get the data of status register
+*
+* Parameters
+*       	[in] void
+* Return
+*   	current status
+*
+*/
+u8 TWI_getStatus(void);
 
 
-#endif /* TWI_H_ */
+#endif /* I2C_H_ */
