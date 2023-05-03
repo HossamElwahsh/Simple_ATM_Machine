b41cce5b9f21adfe2f086d1b6fc1fba4c86d6f5f
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-05-03 22:49:05 +0300

    update the EEPROM prototypes

--- Simple_ATM_Machine/CARD_ECU/HAL/eeprom/External_EEPROM.h
+++ Simple_ATM_Machine/CARD_ECU/HAL/eeprom/External_EEPROM.h
@@ -9,6 +9,8 @@
 #ifndef EXTERNAL_EEPROM_H_
 #define EXTERNAL_EEPROM_H_
 
+// #define EEPROM_ERROR 0
+// #define EEPROM_SUCCESS 1
 
 #include "../../MCAL/twi/twi.h"
 #include "../../MCAL/timer/timer_interface.h"
@@ -20,29 +22,30 @@ typedef enum {
 	EEPROM_SUCCESS
 }EN_eepromError_t;
 
-
 #define EEPROM_24C256B
-
+#define MANDATORY_SEQUENCE		 0xA0 
+#define NEDDED_SHIFT_TIMES	     7
 #ifdef EEPROM_24C16B
-	#define BYTE_ADDRESS			0x0045
-	#define MANDATORY_SEQUENCE	     0xA0
-	#define	PAGE_MASK			     0x0700
-	#define NEDDED_SHIFT_TIMES	     7
-	u8 EEPROM_writeByte(u16 u16_l_byteAddress, u8 u8_l_byteData);
-	u8 EEPROM_readByte(u16 u16_l_byteAddress, u8* u8_l_byteData);
-#else
-	#define BYTE_ADDRESS			 0x011
-	#define MANDATORY_SEQUENCE_256   0xA0   // 10100 
-	#define	PAGE_MASK_256			 0x0600
-	#define NEDDED_SHIFT_TIMES_256	 7	
-	#define SHIFT_THE_FIRST_BYTE	 8	
-	u8 EEPROM_writeByte(u16 u16_l_byteAddress, u8 u8_l_byteData);
-	u8 EEPROM_readByte(u16 u16_l_byteAddress, u8* u8_l_byteData);
-#endif 
-
-void EEPROM_init(void);
+#define BYTE_ADDRESS			0x0311
+#define MANDATORY_SEQUENCE	     0xA0
+#define	PAGE_MASK			     0x0700
+u8 EEPROM_writeByte(u16 u16_l_byteAddress, u8 u8_l_byteData);
+u8 EEPROM_readByte(u16 u16_l_byteAddress, u8* u8_l_byteData);
 u8* EEPROM_readArray(u16 u16_l_byteAddress);
-EN_eepromError_t EEPROM_writeArray(u16 u16_l_byteAddress, u8* pstr);
+u8 EEPROM_writeArray(u16 u16_l_byteAddress, u8* pstr);
+#else
+#define BYTE_ADDRESS			 0x6D18
+#define MANDATORY_SEQUENCE		 0xA0   // 10100 
+#define	PAGE_MASK_256			 0x00FF
+#define NEDDED_SHIFT_TIMES_256	 7	
+#define SHIFT_THE_FIRST_BYTE	 8	
 
- 
+u8 EEPROM_writeByte(u16 u16_l_byteAddress, u8 u8_l_byteData);
+u8 EEPROM_readByte(u16 u16_l_byteAddress, u8* u8_l_byteData);
+u8* EEPROM_readArray(u16 u16_l_byteAddress);
+u8 EEPROM_writeArray(u16 u16_l_byteAddress, u8* pstr);
+#endif
+void EEPROM_init(void);
+
+
 #endif /* EXTERNAL_EEPROM_H_ */
