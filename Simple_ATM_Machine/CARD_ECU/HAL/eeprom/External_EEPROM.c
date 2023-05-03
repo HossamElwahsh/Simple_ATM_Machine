3d88dd3fc9e14e8acc5813f79cb3ca8f2c362601
Mahmoud Mowafey <mahmsa37@gmail.com>
2023-05-03 16:19:22 +0300

    remove duplicate definition

--- Simple_ATM_Machine/CARD_ECU/HAL/eeprom/External_EEPROM.c
+++ Simple_ATM_Machine/CARD_ECU/HAL/eeprom/External_EEPROM.c
@@ -4,81 +4,118 @@
  * Author : Mahmoud Mowafey
  *
 */
-
 #include "External_EEPROM.h"
 
-
-void EEPROM_Init(void)
+void EEPROM_init(void)
 {
-	TWI_Init(); // just initialize the TWI module inside the MC
+	TWI_init(); // just initialize the TWI module inside the MC
 }
 
 #ifdef EEPROM_24C16B
 
-u8 EEPROM_Write_Byte(u16 u16addr, u8 u8data)
+EN_eepromError_t EEPROM_writeByte(u16 u16_l_byteAddress, u8 u8_l_byteData)
 {
-	TWI_Start();
-	if (TWI_Get_Status() != TW_START)
+	TWI_start();
+	if (TWI_getStatus() != TW_START)
+	{
 		return EEPROM_ERROR;
-	
+	}
 	//write the device address and so we need to get A8 A9 A10 address bits and R/W=0 (write)
-	TWI_Write((u8)(0xA0 | ((u16addr & 0x0700)>>7)));
-	if (TWI_Get_Status() != TW_MT_SLA_W_ACK)
+	TWI_write((u8)((MANDATORY_SEQUENCE) | ((u16_l_byteAddress & PAGE_MASK) >> NEDDED_SHIFT_TIMES)));
+	if (TWI_getStatus() != TW_MT_SLA_W_ACK)
+	{
 		return EEPROM_ERROR;
-	
+	}
 	//send the required location address
-	TWI_Write((u8)(u16addr));
-	if (TWI_Get_Status() != TW_MT_DATA_ACK)
+	TWI_write((u8)(u16_l_byteAddress));
+	if (TWI_getStatus() != TW_MT_DATA_ACK)
+	{
 		return EEPROM_ERROR;
-	
+	}
 	//write byte to eeprom
-	TWI_Write(u8data);
-	if (TWI_Get_Status() != TW_MT_DATA_ACK)
+	TWI_write(u8_l_byteData);
+	if (TWI_getStatus() != TW_MT_DATA_ACK)
+	{
 		return EEPROM_ERROR;
-	
-	TWI_Stop();
-	
+	}
+	TWI_stop();
+
 	return EEPROM_SUCCESS;
 }
 
-u8 EEPROM_Read_Byte(u16 u16addr, u8 *u8data)
+EN_eepromError_t EEPROM_readByte(u16 u16_l_byteAddress, u8* u8_l_byteData)
 {
-	TWI_Start();
-	if (TWI_Get_Status() != TW_START)
+	TWI_start();
+	if (TWI_getStatus() != TW_START)
+	{
 		return EEPROM_ERROR;
-	
+	}
 	//write the device address and so we need to get A8 A9 A10 address bits and R/W=0 (write)
-	TWI_Write((u8)((0xA0) | ((u16addr & 0x0700)>>7)));
-	if (TWI_Get_Status() != TW_MT_SLA_W_ACK)
+	TWI_write((u8)((MANDATORY_SEQUENCE) | ((u16_l_byteAddress & PAGE_MASK) >> NEDDED_SHIFT_TIMES)));
+	if (TWI_getStatus() != TW_MT_SLA_W_ACK)
+	{
 		return EEPROM_ERROR;
-	
+	}
 	//send the required location address
-	TWI_Write((u8)(u16addr));
-	if (TWI_Get_Status() != TW_MT_DATA_ACK)
+	TWI_write((u8)(u16_l_byteAddress));
+	if (TWI_getStatus() != TW_MT_DATA_ACK)
+	{
 		return EEPROM_ERROR;
-	
+	}
 	//send a repeated start
-	TWI_Start();
-	if (TWI_Get_Status() != TW_REP_START)
+	TWI_start();
+	if (TWI_getStatus() != TW_REP_START)
+	{
 		return EEPROM_ERROR;
-	
+	}
 	//write the device address and so we need to get A8 A9 A10 address bits and R/W=1 (Read)
-	TWI_Write((u8)((0xA0) | ((u16addr & 0x0700)>>7) | 1));
-	if (TWI_Get_Status() != TW_MT_SLA_R_ACK)
+	TWI_write((u8)((MANDATORY_SEQUENCE) | ((u16_l_byteAddress & PAGE_MASK) >> NEDDED_SHIFT_TIMES) | 1));
+	if (TWI_getStatus() != TW_MT_SLA_R_ACK)
+	{
 		return EEPROM_ERROR;
-	
-	*u8data = TWI_Read_With_NACK();
-	if (TWI_Get_Status() != TW_MR_DATA_NACK)
+	}
+	*u8_l_byteData = TWI_readWithNAck();
+	if (TWI_getStatus() != TW_MR_DATA_NACK)
+	{
 		return EEPROM_ERROR;
-	
-	TWI_Stop();
+	}
+	TWI_stop();
 	return EEPROM_SUCCESS;
 }
 
+EN_eepromError_t EEPROM_writeArray(u16 u16_l_byteAddress, u8* pstr)
+{
+	u8 i = 0, x = 0;
+	u16 j = u16_l_byteAddress;
+	while (pstr[i] != '\0')
+	{
+		x = EEPROM_writeByte(j++, pstr[i++]);
+		TIMER_delay_ms(10);
+		if (0 == x)
+			return EEPROM_ERROR;
+	}
+	EEPROM_writeByte(j, '\0');
+	TIMER_delay_ms(10);
+	return EEPROM_SUCCESS;
+}
+
+u8* EEPROM_readArray(u16 u16_l_byteAddress)
+{
+	u8 i = 0;
+	static u8 arr[20];
+	do {
+		EEPROM_readByte(u16_l_byteAddress, &arr[i]);
+		TIMER_delay_ms(10);
+		u16_l_byteAddress++;
+		i++;
+	} while ((arr[i - 1] != '\0') && (i != 19));
+	arr[i] = '\0';
+	return arr;
+}
 
 #else
 
-EN_eepromError_t EEPROM_readByte(u16 u16_l_byteAddress, u8* u8_l_byteData)
+EN_eepromError_t EEPROM_writeByte(u16 u16_l_byteAddress, u8 u8_l_byteData)
 {
 	TWI_start();
 	if (TWI_getStatus() != TW_START)
@@ -109,29 +146,9 @@ EN_eepromError_t EEPROM_readByte(u16 u16_l_byteAddress, u8* u8_l_byteData)
 	{
 		return EEPROM_ERROR;
 	}
-	//send a repeated start
-	TWI_start();
-	if (TWI_getStatus() != TW_REP_START)
-	{
-		return EEPROM_ERROR;
-	}
-	//Send the Device Address and R/W=0 bit
-	TWI_write((u8)((MANDATORY_SEQUENCE_256) | ((u16_l_byteAddress & PAGE_MASK_256) >> NEDDED_SHIFT_TIMES_256) | 1));
-	if (TWI_getStatus() != TW_MT_SLA_W_ACK)
-	{
-		return EEPROM_ERROR;
-	}
-	//Read the data from the eeprom	with NACK
-	*u8_l_byteData = TWI_readWithNAck();
-	if (TWI_getStatus() != TW_MR_DATA_NACK)
-	{
-		return EEPROM_ERROR;
-	}
-
 	TWI_stop();
 	return EEPROM_SUCCESS;
 }
-
 
 EN_eepromError_t EEPROM_readByte(u16 u16_l_byteAddress, u8* u8_l_byteData)
 {
@@ -189,39 +206,6 @@ EN_eepromError_t EEPROM_readByte(u16 u16_l_byteAddress, u8* u8_l_byteData)
 
 
 #endif
-
-u8 EEPROM_Write_Array(u16 u16addr, u8 *pstr)
-{
-	u8 i=0,x=0;
-	u16 j = u16addr;
-	while(pstr[i] != '\0')
-	{
-		x = EEPROM_Write_Byte(j++,pstr[i++]);
-		TIMER_delay_ms(10);
-		if(0==x)
-			return EEPROM_ERROR;
-	}
-	EEPROM_Write_Byte(j,'\0');
-	TIMER_delay_ms(10);
-	return EEPROM_SUCCESS;
-}
-u8 * EEPROM_Read_Array(u16 address)
-{
-	u8 i=0;
-	u8 *pstr;
-	static u8 arr[20];
-	do{
-		EEPROM_Read_Byte(address,&arr[i]);
-		TIMER_delay_ms(10);
-		address++;
-		i++;
-	}while( ( arr[i-1] != '\0' ) && ( i != 19 ) );
-	arr[i] = '\0';
-	return arr;
-}
-
-
-
 
 
 u8 decimal_to_hex(u8 val)
